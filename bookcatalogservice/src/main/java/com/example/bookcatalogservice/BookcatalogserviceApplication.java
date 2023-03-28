3e851d173dce1c7cbc667f0db573301d7039181c
Montu Sharma <you@example.com>
2023-03-28 21:43:29 +0530

    Git intialize and Added library-admin, bookscatalog,users microservice

---
+++ bookcatalogservice/src/main/java/com/example/bookcatalogservice/BookcatalogserviceApplication.java
@@ -0,0 +1,15 @@
+package com.example.bookcatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BookcatalogserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookcatalogserviceApplication.class, args);
+	}
+
+}
